0e02d84e6d796e4cce194eb666c4b678062799d1
Alex <slizzle@alexs-MacBook-Pro.local>
2025-06-01 21:59:04 -0700

    Add update and delete functions to CRUD layer

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from typing import List
 from sqlmodel import Session, select
 from .database import engine
@@ -15,3 +16,25 @@ def create_asset(*, asset: AssetCreate) -> Asset:
 def get_assets() -> List[Asset]:
     with Session(engine) as session:
         return session.exec(select(Asset)).all()
+
+def update_asset(asset_id: int, asset_data: AssetCreate) -> Optional[Asset]:
+    with Session(engine) as session:
+        asset = session.get(Asset, asset_id)
+        if not asset:
+            return None
+        asset.symbol = asset_data.symbol
+        asset.quantity = asset_data.quantity
+        asset.average_price = asset_data.average_price
+        session.add(asset)
+        session.commit()
+        session.refresh(asset)
+        return asset
+
+def delete_asset(asset_id: int) -> bool:
+    with Session(engine) as session:
+        asset = session.get(Asset, asset_id)
+        if not asset:
+            return False
+        session.delete(asset)
+        session.commit()
+        return True
